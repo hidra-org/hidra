2764b7680462ee9b33623f2ceb84ee0fe599463a
Manuela Kuhn <manuela.kuhn@desy.de>
2016-01-27 16:40:56 +0100

    Added tests for zmq authentication

---
+++ test/zmq_test/auth_client.py
@@ -0,0 +1,19 @@
+import zmq
+import sys
+
+port = "5556"
+#ip="localhost"
+#ip="*"
+ip="zitpcx19282.desy.de"
+
+context = zmq.Context()
+print "Connecting to server..."
+socket = context.socket(zmq.PULL)
+socket.connect("tcp://"+ ip + ":" + port)
+
+
+#  Do 10 requests, waiting each time for a response
+for request in range (1,10):
+    #  Get the reply.
+    message = socket.recv_multipart()
+    print "received reply ", request, "[", message, "]"
---
+++ test/zmq_test/auth_server.py
@@ -0,0 +1,37 @@
+import zmq
+from zmq.auth.thread import ThreadAuthenticator
+import time
+import sys
+
+port = "5556"
+ip = "*"
+#ip="zitpcx19282.desy.de"
+
+context = zmq.Context()
+socket = context.socket(zmq.PUSH)
+socket.zap_domain = b'global'
+socket.bind("tcp://" + ip + ":%s" % port)
+
+auth = ThreadAuthenticator(context)
+whitelist = "131.169.185.121"
+#whitelist = "131.169.185.34"
+auth.start()
+auth.allow(whitelist)
+
+
+while True:
+    try:
+        message = ["World"]
+        print "Send: ", message
+        res = socket.send_multipart(message, copy=False, track=True)
+        if res.done:
+            print "res: done"
+        else:
+            print "res: waiting"
+            res.wait()
+            print "res: waiting..."
+        print "sleeping..."
+        time.sleep (1)
+        print "sleeping...done"
+    finally:
+        auth.stop()
