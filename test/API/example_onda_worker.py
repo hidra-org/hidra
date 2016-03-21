9bd1dfa66a3a835ca9780685bf6fc13ddcc77e35
Manuela Kuhn <manuela.kuhn@desy.de>
2016-03-21 14:43:34 +0100

    Added master-worker example for onda

---
+++ test/API/example_onda_worker.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import time
+
+
+BASE_PATH   = os.path.dirname ( os.path.dirname ( os.path.dirname ( os.path.realpath ( __file__ ) ) ) )
+API_PATH    = BASE_PATH + os.sep + "APIs"
+
+if not API_PATH in sys.path:
+    sys.path.append ( API_PATH )
+del API_PATH
+del BASE_PATH
+
+from dataTransferAPI import dataTransfer
+
+
+class worker():
+    def __init__(self, id, signalHost, port):
+
+        self.id    = id
+        self.port  = port
+
+        self.query = dataTransfer("queryNext", signalHost)
+
+        print "start dataTransfer on port", str(port)
+        self.query.start(port)
+
+
+    def run(self):
+        while True:
+            try:
+                print "worker-" + str(self.id) + ": waiting"
+                [metadata, data] = self.query.get()
+                time.sleep(0.1)
+            except:
+                break
+
+            print "worker-" + str(self.id), "metadata", metadata["filename"]
+        #    print "data", str(data)[:10]
+
+
+    def stop(self):
+        self.query.stop()
+
+
+    def __exit__(self):
+        self.stop()
+
+
+    def __del__(self):
+        self.stop()
+
+
+print
+print "==== TEST: Query for the newest filename ===="
+print
+
+signalHost = "zitpcx19282.desy.de"
+port = "50104"
+
+w = worker(4, signalHost, port)
+
+try:
+    w.run()
+finally:
+    w.stop()
+
+
+print
+print "==== TEST END: Query for the newest filename ===="
+print
+
+
