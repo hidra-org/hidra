8cc8a62df89fe3602843ea443e99bb11fa820482
Manuela Kuhn <manuela.kuhn@desy.de>
2016-04-15 15:34:16 +0200

    First version of start/stop file

---
+++ dataTransfer.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+INSTANZ="DataManager"
+Pidfile=/space/projects/zeromq-data-transfer/DataManager.pid
+
+DATAMANAGER=/space/projects/zeromq-data-transfer/src/sender/DataManager.py
+
+if [ -f $Pidfile ]
+then
+	Pid=`cat $Pidfile`
+fi
+
+
+start() {
+		if [ -f $Pidfile ] ; then
+				if test `ps -e | grep -c $Pid` = 1; then
+						echo "Not starting $INSTANZ - instance already running with PID: $Pid"
+				else
+						echo "Starting $INSTANZ"
+						nohup /usr/bin/python ${DATAMANAGER} &> /dev/null &
+						echo $! > $Pidfile
+				fi
+		else
+				echo "Starting $INSTANZ"
+				nohup /usr/bin/python ${DATAMANAGER} &> /dev/null &
+				echo $! > $Pidfile
+		fi
+}
+
+
+stop()
+{
+		if [ -f $Pidfile ] ; then
+				echo "Stopping $INSTANZ"
+#                pid=$(ps -ef | grep '${DATAMANAGER}' | awk '{ print $2 }')
+                echo ${pid}
+                kill -15 $Pid
+                rm $Pidfile
+#                echo $!
+		else
+				echo "Cannot stop $INSTANZ - no Pidfile found!"
+		fi
+}
+
+status()
+{
+
+		if [ -f $Pidfile ] ; then
+				if test `ps -e | grep -c $Pid` = 0; then
+						echo "$INSTANZ not running"
+				else
+						echo "$INSTANZ running with PID: [$Pid]"
+				fi
+		else
+				echo "$Pidfile does not exist! Cannot process $INSTANZ status!"
+				exit 1
+		fi
+    }
+
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        sleep 5
+        start
+        ;;
+    status)
+        status
+        ;;
+    *)
+        echo "Usage: $0 {start | stop | restart | status}"
+        exit 1
+esac
+exit 0
+
+
+
