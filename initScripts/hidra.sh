7bf83aed63dbc5ade40ec309dde936dd6b9f5873
Manuela Kuhn <manuela.kuhn@desy.de>
2016-11-30 17:17:41 +0100

    Fixed pid file removal in initscript for debian/ubuntu systems

--- initScripts/hidra.sh
+++ initScripts/hidra.sh
@@ -99,71 +99,109 @@ if [ -f /etc/redhat-release -o -f /etc/centos-release ] ; then
 elif [ -f /etc/debian_version ] ; then
 # Debian and Ubuntu
 
-## Exit if the package is not installed
+    # Exit if the package is not installed
 #    [ -x "$DAEMON" ] || exit 0
 
-# Load the VERBOSE setting and other rcS variables
+    # Load the VERBOSE setting and other rcS variables
 #    . /lib/init/vars.sh
 
-# Define LSB log_* functions.
-# Depend on lsb-base (>= 3.2-14) to ensure that this file is present
-# and status_of_proc is working.
+    # Define LSB log_* functions.
+    # Depend on lsb-base (>= 3.2-14) to ensure that this file is present
+    # and status_of_proc is working.
     . /lib/lsb/init-functions
 
 #    ln -s "$DAEMON" "$NAME"
-#
-# Function that starts the daemon/service
-#
+
+    #
+    # Function that starts the daemon/service
+    #
     do_start()
     {
-        # Return
-        #   0 if daemon has been started
-        #   1 if daemon was already running
-        #   2 if daemon could not be started
-        start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --exec $DAEMON --test > /dev/null \
-            || return 1
+        # Checked the PID file exists and check the actual status of process
+        if [ -e $PIDFILE ]; then
+            status_of_proc -p $PIDFILE $DAEMON "$NAME" > /dev/null && status="1" || status="$?"
+            # If the status is SUCCESS then don't need to start again.
+            if [ $status = "1" ]; then
+                log_daemon_msg "$NAME is already running"
+                return 0
+                exit
+            fi
+        fi
 
-#        start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile \
-        start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background \
-            --startas $DAEMON -- $DAEMON_ARGS \
-            || return 2
+        # Start the daemon with the help of start-stop-daemon
+        # possible exit status
+        # 0      The requested action was performed. If --oknodo was specified,
+        #        it's also possible that nothing had to be done.  This can
+        #        happen when --start was specified and a matching process was
+        #        already running, or when --stop was specified and there were
+        #        no matching processes.
+        # 1      If --oknodo was not specified and nothing was done.
+        # 2      If --stop and --retry were specified, but the end of the
+        #        schedule was reached and the processes were still running.
+        # 3      Any other error.
+        # (--oknodo: If the a process exists start-stop-daemon exits with error
+        #  status 0 instead of 1)
+        if start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background \
+            --startas $DAEMON -- $DAEMON_ARGS ; then
+            return 0
+        else
+            return 1
+        fi
     }
 
-#
-# Function that stops the daemon/service
-#
-    do_stop()
+    cleanup()
     {
-        # Return
-        #   0 if daemon has been stopped
-        #   1 if daemon was already stopped
-        #   2 if daemon could not be stopped
-        #   other if a failure occurred
-        start-stop-daemon --stop --quiet --pidfile $PIDFILE #--name $NAME
-#        start-stop-daemon --stop --quiet --retry=TERM/180/KILL/5 --pidfile $PIDFILE
-
-        RETVAL="$?"
-        [ "$RETVAL" = 2 ] && return 2
-        # Wait for children to finish too if this is a daemon that forks
-        # and if the daemon is only ever run from this initscript.
-        # If the above conditions are not satisfied then add some other code
-        # that waits for the process to drop all resources that could be
-        # needed by services started subsequently.  A last resort is to
-        # sleep for some time.
-        start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
-        [ "$?" = 2 ] && return 2
-
         SOCKETID=`cat $PIDFILE`
-        rm -f "${IPCPATH}/${SOCKETID}"*
+        /bin/rm -rf "${IPCPATH}/${SOCKETID}"*
 
         # Many daemons don't delete their pidfiles when they exit.
-        rm -f $PIDFILE
-        return "$RETVAL"
+        /bin/rm -rf $PIDFILE
+        return 0
+
     }
 
-#
-# Function that sends a SIGHUP to the daemon/service
-#
+    #
+    # Function that stops the daemon/service
+    #
+    do_stop()
+    {
+        # Stop the daemon.
+        if [ -e $PIDFILE ]; then
+#            status_of_proc $NAME $NAME && exit 0 || exit $?
+            status_of_proc $NAME "$NAME" > /dev/null && status="0" || status="$?"
+            if [ "$status" = 0 ]; then
+                start-stop-daemon --stop --quiet --pidfile $PIDFILE #--name $NAME
+#                start-stop-daemon --stop --quiet --retry=TERM/180/KILL/5 --pidfile $PIDFILE
+                daemon_status="$?"
+                if [ "$daemon_status" = 2 ]; then
+                    cleanup
+                    return 1
+                elif [ "$daemon_status" = 0 ]; then
+                    cleanup
+                    return 0
+                fi
+
+                # Wait for children to finish too if this is a daemon that forks
+                # and if the daemon is only ever run from this initscript.
+                # If the above conditions are not satisfied then add some other code
+                # that waits for the process to drop all resources that could be
+                # needed by services started subsequently.  A last resort is to
+                # sleep for some time.
+                start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
+                [ "$?" = 2 ] && cleanup && return 1
+
+                cleanup
+            else
+                cleanup
+            fi
+        else
+            log_daemon_msg "$NAME is not running"
+        fi
+    }
+
+    #
+    # Function that sends a SIGHUP to the daemon/service
+    #
     do_reload()
     {
         # If the daemon can reload its configuration without
@@ -175,22 +213,22 @@ elif [ -f /etc/debian_version ] ; then
 
     case "$1" in
         start)
-            [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
+            log_daemon_msg "Starting $NAME"
             do_start
             case "$?" in
-                0|1) [ "$VERBOSE" != no ] && log_end_msg 0
+                0) log_end_msg 0
                     ;;
-                2) [ "$VERBOSE" != no ] && log_end_msg 1
+                *) log_end_msg 1
                     ;;
             esac
             ;;
         stop)
-            [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
+            log_daemon_msg "Stopping $NAME"
             do_stop
             case "$?" in
-                0|1) [ "$VERBOSE" != no ] && log_end_msg 0
+                0) log_end_msg 0
                     ;;
-                2) [ "$VERBOSE" != no ] && log_end_msg 1
+                *) log_end_msg 1
                     ;;
             esac
             ;;
@@ -210,15 +248,25 @@ elif [ -f /etc/debian_version ] ; then
             # 'force-reload' alias
 
             log_daemon_msg "Restarting $DESC" "$NAME"
+            log_daemon_msg "Stopping $DESC" "$NAME"
             do_stop
-            sleep 1
-            case "$?" in
-                0|1)
+            stop_status="$?"
+            case "$stop_status" in
+                0) log_end_msg 0
+                    ;;
+                *) log_end_msg 1
+                    ;;
+            esac
+            sleep 3
+            case "$stop_status" in
+                0)
+                    log_daemon_msg "Starting $NAME"
                     do_start
                     case "$?" in
-                        0) log_end_msg 0 ;;
-                        1) log_end_msg 1 ;; # Old process is still running
-                        *) log_end_msg 1 ;; # Failed to start
+                        0) log_end_msg 0
+                            ;;
+                        *) log_end_msg 1
+                            ;;
                     esac
                     ;;
                 *)
