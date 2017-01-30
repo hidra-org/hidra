1ef9afcd0a3acb28d4903faadef32cb26a2ec960
Manuela Kuhn <manuela.kuhn@desy.de>
2017-01-30 12:09:11 +0100

    Modified paths in sender __init__ file

--- src/sender/__init__.py
+++ src/sender/__init__.py
@@ -3,27 +3,19 @@ import sys
 
 # path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 try:
-    BASE_PATH = os.path.dirname(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.realpath(__file__))))
+    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 except:
-    BASE_PATH = os.path.dirname(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.realpath('__file__'))))
-#    BASE_PATH = os.path.dirname(
-#        os.path.dirname(
+    CURRENT_DIR = os.path.dirname(os.path.realpath('__file__'))
 #           os.path.dirname(
 #               os.path.abspath(sys.argv[0]))))
-#    BASE_PATH = os.path.dirname(
-#        os.path.dirname(
 #           os.path.dirname(
 #               os.path.realpath(sys.argv[0])))))
 
+
+BASE_PATH = os.path.dirname(os.path.dirname(CURRENT_DIR))
 SHARED_PATH = os.path.join(BASE_PATH, "src", "shared")
-EVENTDETECTOR_PATH = os.path.join(BASE_PATH, "src", "sender", "eventdetectors")
-DATAFETCHER_PATH = os.path.join(BASE_PATH, "src", "sender", "datafetchers")
+EVENTDETECTOR_PATH = os.path.join(CURRENT_DIR, "eventdetectors")
+DATAFETCHER_PATH = os.path.join(CURRENT_DIR, "datafetchers")
 
 if SHARED_PATH not in sys.path:
     sys.path.append(SHARED_PATH)
