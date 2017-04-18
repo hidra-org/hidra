dc034a8e84966e1767f426373a67b361c26a76bc
Manuela Kuhn <manuela.kuhn@desy.de>
2017-04-18 16:31:29 +0200

    Adjusted host names in mapping for control API

--- src/APIs/hidra/_constants.py
+++ src/APIs/hidra/_constants.py
@@ -4,51 +4,51 @@ connection_list = {
         "port": 51000
     },
     "p01": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p01",
         "port": 51000
     },
     "p02.1": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p02-1",
         "port": 51000
     },
     "p02.2": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p02-2",
         "port": 51000
     },
     "p03": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p03",
         "port": 51000
     },
     "p04": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p04",
         "port": 51000
     },
     "p05": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p05",
         "port": 51000
     },
     "p06": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p06",
         "port": 51000
     },
     "p07": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p07",
         "port": 51000
     },
     "p08": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p08",
         "port": 51000
     },
     "p09": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p09",
         "port": 51000
     },
     "p10": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p10",
         "port": 51000
     },
     "p11": {
-        "host": "asap3-bl-prx07",
+        "host": "asap3-p11",
         "port": 51000
     },
 }
