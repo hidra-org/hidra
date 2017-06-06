a9590c6f9963df352631beab18dd7aff815181cb
Manuela Kuhn <manuela.kuhn@desy.de>
2017-06-06 09:49:57 +0200

    Added regex example to regex test

--- test/codingTests/regular_expression.py
+++ test/codingTests/regular_expression.py
@@ -1,6 +1,6 @@
 import re
 
-regex = [".*", ".*[tif|cbf]$", ".*(tif|cbf)$", ".*[.]$", "m1_.*.nxs$", ".*(log|tif|cbf|nxs)$"]
+regex = [".*", ".*[tif|cbf]$", ".*(tif|cbf)$", ".*[.]$", "m1_.*.nxs$", ".*(log|tif|cbf|nxs)$", ".*m2.*nxs$"]
 strings = ["m1_m123.nxs", "m2_m123.nxs", "m1_m123.cbf", "m1_m123.tif", "m1_m123.tfi", "abc.bin"]
 
 for r in regex:
