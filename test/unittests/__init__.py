81ba83a19a231c302ae960d8723b8970141a5156
Manuela Kuhn <manuela.kuhn@desy.de>
2017-06-19 17:01:39 +0200

    Added unittest for inotifyx_events

---
+++ test/unittests/__init__.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+try:
+    BASE_PATH = os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.realpath(__file__))))
+except:
+    BASE_PATH = os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.realpath(sys.argv[0]))))
+
+SHARED_PATH = os.path.join(BASE_PATH, "src", "shared")
+EVENT_DETECTOR_PATH = os.path.join(BASE_PATH, "src", "sender",
+                                   "eventdetectors")
+API_PATH = os.path.join(BASE_PATH, "src", "APIs")
+
+if SHARED_PATH not in sys.path:
+    sys.path.append(SHARED_PATH)
+
+if EVENT_DETECTOR_PATH not in sys.path:
+    sys.path.append(EVENT_DETECTOR_PATH)
+
+try:
+    # search in global python modules first
+    from hidra import Transfer  # noqa F401
+except:
+    # then search in local modules
+    if API_PATH not in sys.path:
+        sys.path.append(API_PATH)
