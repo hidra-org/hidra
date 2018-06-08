73db93e815287ae7033d92937aecbd82cb25a5f8
Manuela Kuhn <manuela.kuhn@desy.de>
2018-06-08 11:59:01 +0200

    Generalized test_suite

--- test/unittests/test_suite.py
+++ test/unittests/test_suite.py
@@ -1,18 +1,59 @@
-import unittest
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
 
-from event_detectors.test_inotifyx_events import TestEventDetector as TestInotifyxEvents
-from event_detectors.test_watchdog_events import TestEventDetector as TestWatchdogEvents
-from event_detectors.test_http_events import TestEventDetector as TestHttpEvents
-from event_detectors.test_zmq_events import TestEventDetector as TestZmqEvents
+import unittest
+from importlib import import_module
+import pkgutil
+
+import event_detectors
+
+
+def get_eventdetector_suites():
+    """Collects all available eventdetector tests
+
+    Returns:
+        An array containing all available eventdetector test suites.
+    """
+
+    all_suites = []
+
+    event_detectors_m = "event_detectors"
+    # find all event detector test modules
+    # iter_modules returns: importer, modname, ispkg
+    for _, modname, _ in pkgutil.iter_modules(event_detectors.__path__):
+        # the base class not a test module
+        if modname == "test_eventdetector_base":
+            continue
+
+        # load the test suite
+        module_name = "{}.{}".format(event_detectors_m, modname)
+        module = import_module(module_name).TestEventDetector
+        suite = unittest.TestLoader().loadTestsFromTestCase(module)
+        # this is equivalent to loading one module like this
+        # > from event_detectors.test_inotifyx_events \
+        # >     import TestEventDetector as TestInotifyxEvents
+        # > loader = unittest.TestLoader()
+        # > suite = loader.loadTestsFromTestCase(TestInotifyxEvents)
+
+        # add the test suite
+        all_suites.append(suite)
+
+    return all_suites
+
 
 def suite():
+    """ Collect all tests to be performed as one suite.
 
-    all_suites = [
-        unittest.TestLoader().loadTestsFromTestCase(TestInotifyxEvents),
-        unittest.TestLoader().loadTestsFromTestCase(TestWatchdogEvents),
-        unittest.TestLoader().loadTestsFromTestCase(TestHttpEvents),
-        unittest.TestLoader().loadTestsFromTestCase(TestZmqEvents),
-    ]
+    Returns:
+        A unittest TestSuite instance containing all unittests to be performed.
+    """
+
+    # get the subsuites
+    all_suites = []
+    all_suites += get_eventdetector_suites()
+
+    # combine all subsuites to one big one
     suite = unittest.TestSuite(all_suites)
 
     return suite
@@ -21,4 +62,3 @@ def suite():
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite())
-
