94d74fa5d01c66d04df4b1f3d5b8cb6bb648a8e6
Manuela Kuhn <manuela.kuhn@desy.de>
2018-06-08 09:04:39 +0200

    Migrated functional tests from inotifyx_events into unittests

--- test/unittests/event_detectors/test_inotityx_events.py
+++ test/unittests/event_detectors/test_inotityx_events.py
@@ -1,11 +1,15 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
 import unittest
 import os
 import time
 import logging
 from shutil import copyfile
 
-from __init__ import BASE_DIR
-from test_eventdetector_base import TestEventDetectorBase
+from .__init__ import BASE_DIR
+from .test_eventdetector_base import TestEventDetectorBase
 from inotifyx_events import EventDetector
 
 
@@ -22,8 +26,10 @@ class TestEventDetector(TestEventDetectorBase):
         self.config = {
             "monitored_dir": os.path.join(BASE_DIR, "data", "source"),
             "fix_subdirs": ["commissioning", "current", "local"],
-            "monitored_events": {"IN_CLOSE_WRITE": [".tif", ".cbf", ".file"],
-                                 "IN_MOVED_TO": [".log"]},
+            "monitored_events": {
+                "IN_CLOSE_WRITE": [".tif", ".cbf", ".file"],
+                "IN_MOVED_TO": [".log"]
+            },
             # "event_timeout": 0.1,
             "history_size": 0,
             "use_cleanup": False,
@@ -43,6 +49,8 @@ class TestEventDetector(TestEventDetectorBase):
                                              self.target_relative_path)
         # TODO why is this needed?
         self.target_file_base += os.sep
+
+        self.eventdetector = None
 
     def _start_eventdetector(self):
         self.eventdetector = EventDetector(self.config, self.log_queue)
@@ -71,14 +79,103 @@ class TestEventDetector(TestEventDetectorBase):
             self.assertDictEqual(event_list[0],
                                  expected_result_dict)
 
+    # this should not be executed automatically only if needed for debugging
+    @unittest.skip("Only needed for debugging")
+    def test_memory_usage(self):
+        import resource
+        import gc
+        # don't care about stuff that would be garbage collected properly
+        gc.collect()
+        from guppy import hpy
+
+        self._init_logging(loglevel="info")
+        self._create_dir(self.target_file_base)
+        self._start_eventdetector()
+
+#        self.config["use_cleanup"] = True
+#        self.config["monitored_events"] = {
+#            "Some_supid_event": [".tif", ".cbf", ".file"]
+#        }
+#        self.config["time_till_closed"] = 0.2
+#        self.config["action_time"] = 0.5
+
+        self.start = 100
+        self.stop = 30000
+        steps = 10
+
+        memory_usage_old = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        print("Memory usage at start: {} (kb)".format(memory_usage_old))
+
+        hp = hpy()
+        hp.setrelheap()
+
+        step_loop = (self.stop - self.start) / steps
+        print("Used steps:", steps)
+
+        try:
+            for s in range(steps):
+                start = self.start + s * step_loop
+                stop = start + step_loop
+#                print ("start=", start, "stop=", stop)
+                for i in range(start, stop):
+
+                    target_file = "{}{}.cbf".format(self.target_file_base, i)
+                    copyfile(self.source_file, target_file)
+                    time.sleep(0.1)
+
+                    if i % 100 == 0:
+                        print("copy index {}".format(i))
+                        event_list = self.eventdetector.get_new_event()
+
+#                    time.sleep(0.5)
+
+                memory_usage_new = (
+                    resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+                print("Memory usage in iteration {}: {} (kb)"
+                      .format(s, memory_usage_new))
+                if memory_usage_new > memory_usage_old:
+                    memory_usage_old = memory_usage_new
+                    print(hp.heap())
+
+        except KeyboardInterrupt:
+            pass
+        finally:
+            if self.config["use_cleanup"]:
+                time.sleep(4)
+                event_list = self.eventdetector.get_new_event()
+                print("len of event_list={}".format(len(event_list)))
+
+                memory_usage_new = (
+                    resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+                print("Memory usage: {} (kb)".format(memory_usage_new))
+                time.sleep(1)
+
+                event_list = self.eventdetector.get_new_event()
+                print("len of event_list={}".format(len(event_list)))
+
+                memory_usage_new = (
+                    resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+                print("Memory usage: {} (kb)".format(memory_usage_new))
+
+                event_list = self.eventdetector.get_new_event()
+                print("len of event_list={}".format(len(event_list)))
+
+            memory_usage_new = (
+                resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+            print("Memory usage before stop: {} (kb)"
+                  .format(memory_usage_new))
+            time.sleep(5)
+
     def tearDown(self):
-        self.eventdetector.stop()
+        if self.eventdetector is not None:
+            self.eventdetector.stop()
+            self.eventdetector = None
 
         # clean up the created files
         for number in range(self.start, self.stop):
             try:
                 target_file = "{}{}.cbf".format(self.target_file_base, number)
-                print("remove {}".format(target_file))
+                logging.debug("remove {}".format(target_file))
                 os.remove(target_file)
             except OSError:
                 pass
