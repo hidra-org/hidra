48fbf378db4e3e340a9380c470db4e7315bcd4e5
Manuela Kuhn <manuela.kuhn@desy.de>
2018-09-10 14:03:54 +0200

    Fixed beamline to host mapping

--- src/APIs/hidra/_constants.py
+++ src/APIs/hidra/_constants.py
@@ -7,11 +7,11 @@ connection_list = {
         "host": "asap3-p01",
         "port": 51000
     },
-    "p02.1": {
+    "p02-1": {
         "host": "asap3-p02-1",
         "port": 51000
     },
-    "p02.2": {
+    "p02-2": {
         "host": "asap3-p02-2",
         "port": 51000
     },
@@ -49,6 +49,74 @@ connection_list = {
     },
     "p11": {
         "host": "asap3-p11",
+        "port": 51000
+    },
+    "p21-1": {
+        "host": "asap3-p21-1",
+        "port": 51000
+    },
+    "p21-2": {
+        "host": "asap3-p21-2",
+        "port": 51000
+    },
+    "p22": {
+        "host": "asap3-p22",
+        "port": 51000
+    },
+    "p64": {
+        "host": "asap3-p64",
+        "port": 51000
+    },
+    "p65": {
+        "host": "asap3-p65",
+        "port": 51000
+    },
+    "bl1": {
+        "host": "asap3-bl1",
+        "port": 51000
+    },
+    "bl2": {
+        "host": "asap3-bl2",
+        "port": 51000
+    },
+    "bl3": {
+        "host": "asap3-bl3",
+        "port": 51000
+    },
+    "pg1": {
+        "host": "asap3-pg1",
+        "port": 51000
+    },
+    "pg2": {
+        "host": "asap3-pg2",
+        "port": 51000
+    },
+    "thz": {
+        "host": "asap3-thz",
+        "port": 51000
+    },
+    "fl21": {
+        "host": "asap3-fl21",
+        "port": 51000
+    },
+    "fl22": {
+        "host": "asap3-fl22",
+        "port": 51000
+    },
+    "fl23": {
+        "host": "asap3-fl23",
+        "port": 51000
+    },
+    "fl24": {
+        "host": "asap3-fl24",
+        "port": 51000
+    },
+    "fl25": {
+        "host": "asap3-fl25",
+        "port": 51000
+    },
+    "fl26": {
+        "host": "asap3-fl26",
         "port": 51000
     },
 }
