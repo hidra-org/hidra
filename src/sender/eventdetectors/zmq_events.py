bf95cf35dff025097cdb70e9263b32a749b281ec
Manuela Kuhn <manuela.kuhn@desy.de>
2018-10-29 15:47:15 +0100

    Cleaned up zmq events

--- src/sender/eventdetectors/zmq_events.py
+++ src/sender/eventdetectors/zmq_events.py
@@ -1,10 +1,39 @@
+# Copyright (C) 2015  DESY, Manuela Kuhn, Notkestr. 85, D-22607 Hamburg
+#
+# HiDRA is a generic tool set for high performance data multiplexing with
+# different qualities of service and based on Python and ZeroMQ.
+#
+# This software is free: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Manuela Kuhn <manuela.kuhn@desy.de>
+#
+
+"""
+This module implements an event detector to be used together with the hidra
+ingest API.
+"""
+
+# pylint: disable=broad-except
+
+from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-from __future__ import absolute_import
 
+from collections import namedtuple
 import json
 import zmq
-from collections import namedtuple
 
 from eventdetectorbase import EventDetectorBase
 import utils
@@ -144,6 +173,9 @@ def get_endpoints(ipc_addresses, tcp_addresses):
 
 
 class EventDetector(EventDetectorBase):
+    """
+    Implementation of the event detector to be used with the ingest API.
+    """
 
     def __init__(self, config, log_queue):
 
@@ -215,7 +247,7 @@ class EventDetector(EventDetectorBase):
 
         if event_message[0] == b"CLOSE_FILE":
             event_message_list = [
-                event_message for i in range(self.config["number_of_streams"])
+                event_message for _ in range(self.config["number_of_streams"])
             ]
         else:
             event_message_list = [json.loads(event_message[0].decode("utf-8"))]
@@ -238,5 +270,5 @@ class EventDetector(EventDetectorBase):
                 self.context.destroy(0)
                 self.context = None
                 self.log.info("Closing ZMQ context...done.")
-            except:
+            except Exception:
                 self.log.error("Closing ZMQ context...failed.", exc_info=True)
