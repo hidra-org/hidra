f56271392d2b24bbd1b38335ac1aa9bf08f84dde
Manuela Kuhn <manuela.kuhn@desy.de>
2018-12-05 15:18:19 +0100

    Some zmq tracker tests

---
+++ test/coding_tests/zmq_test/pushpull_send.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import zmq
+import time
+import socket
+
+port = "50300"
+ip=socket.getfqdn()
+
+context = zmq.Context()
+socket = context.socket(zmq.PUSH)
+socket.connect("tcp://{0}:{1}".format(ip, port))
+
+print("using zmq version", zmq.__version__)
+
+while True:
+    message = b"World"
+    print("Send: ", message)
+    res = socket.send(message, copy=False, track=True)
+
+    if res.done:
+        print("res: done")
+    else:
+        print("res: waiting")
+        res.wait()
+        print("res: waiting...")
+    print("sleeping...")
+    time.sleep (1)
+    print("sleeping...done")
