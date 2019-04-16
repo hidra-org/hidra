22a8537f7d04a05708af170b39be6671d61e4371
Manuela Kuhn <manuela.kuhn@desy.de>
2019-04-16 14:36:40 +0200

    Fix stop command for redhat init script
    The stop command tried to remove a pidfile which was never created when
    running on redhat systems.

--- initscripts/hidra.sh
+++ initscripts/hidra.sh
@@ -285,10 +285,6 @@ if [ -f /etc/redhat-release -o -f /etc/centos-release ] ; then
 
         if checkpid $HIDRA_PID ; then
             killall -KILL $NAME
-
-            SOCKETID=`cat $PIDFILE`
-
-            rm -f "${IPCDIR}/${SOCKETID}"*
         fi
 
         status ${NAME} > /dev/null 2>&1 && status="1" || status="$?"
