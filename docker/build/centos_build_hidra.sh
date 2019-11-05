7ede6bf61cf0361a93450330de78b3c7d0f31d2a
Manuela Kuhn <manuela.kuhn@desy.de>
2019-11-05 15:25:02 +0100

    Structure centos package build script

--- docker/build/centos_build_hidra.sh
+++ docker/build/centos_build_hidra.sh
@@ -1,76 +1,113 @@
 #!/bin/bash
 
-URL="https://raw.githubusercontent.com/hidra-org/hidra/master/src/APIs/hidra/utils/_version.py"
-VERSION=$(curl -L $URL)
-# cut of the first characters
-VERSION=${VERSION:15}
-VERSION=${VERSION%?}
+get_hidra_version()
+{
+    URL="https://raw.githubusercontent.com/hidra-org/hidra/master/src/APIs/hidra/utils/_version.py"
+    HIDRA_VERSION=$(curl -L $URL)
+    # cut of the first characters
+    HIDRA_VERSION=${HIDRA_VERSION:15}
+    HIDRA_VERSION=${HIDRA_VERSION%?}
+}
 
-MAPPED_DIR=/tmp/hidra_builds/${VERSION}/centos/rpmbuild
-IN_DOCKER_DIR=~/rpmbuild
+download_hidra()
+{
+    # clean up old download
+    if [ -d "$MAPPED_DIR/hidra" ]; then
+        rm -rf $MAPPED_DIR/hidra
+    fi
+
+    # get sources
+    git clone --branch "v$HIDRA_VERSION" https://github.com/hidra-org/hidra.git
+}
+
+prepare_build()
+{
+    cd ${MAPPED_DIR}
+
+    # create rpm structure
+    mkdir -p ${MAPPED_DIR}/BUILD
+    mkdir -p ${MAPPED_DIR}/BUILDROOT
+    mkdir -p ${MAPPED_DIR}/RPMS
+    mkdir -p ${MAPPED_DIR}/SOURCES
+    mkdir -p ${MAPPED_DIR}/SPECS
+    mkdir -p ${MAPPED_DIR}/SRPMS
+
+}
+
+build_docker_image()
+{
+    DOCKER_DIR=$(pwd)
+    DOCKER_IMAGE=centos${CENTOS_VERSION}_build
+    DOCKER_CONTAINER=hidra_build_centos${CENTOS_VERSION}
+    DOCKER_FILE="${MAPPED_DIR}/hidra/docker/build/Dockerfile.build_centos${CENTOS_VERSION}"
+
+    cd ${DOCKER_DIR}
+    if [[ "$(docker images -q ${DOCKER_IMAGE} 2> /dev/null)" == "" ]]; then
+        echo "Building docker image"
+        docker build -f "${DOCKER_FILE}" -t "${DOCKER_IMAGE}" .
+    fi
+}
+
+build_package()
+{
+    echo "Building package"
+    cmd="cd ~/rpmbuild/SPECS; rpmbuild -ba -D 'dist .el7' hidra.spec"
+
+    PASSWD_FILE=/tmp/passwd_x
+    GROUP_FILE=/tmp/group_x
+
+    getent passwd $USER > $PASSWD_FILE
+    echo "$(id -gn):*:$(id -g):$USER" > $GROUP_FILE
+    docker create -it \
+        -v $PASSWD_FILE:/etc/passwd \
+        -v $GROUP_FILE:/etc/group \
+        --userns=host \
+        --net=host \
+        --security-opt no-new-privileges \
+        --privileged \
+        -v ${MAPPED_DIR}:$IN_DOCKER_DIR \
+        --user=$(id -u $USER):$(id -g $USER) \
+        --name ${DOCKER_CONTAINER} \
+        ${DOCKER_IMAGE} \
+        bash
+    docker start ${DOCKER_CONTAINER} > /dev/null && echo "Started container"
+    docker exec --user=$(id -u $USER):$(id -g $USER) ${DOCKER_CONTAINER} sh -c "$cmd" && success=true
+    docker stop ${DOCKER_CONTAINER} > /dev/null && echo "Stopped container"
+    docker rm ${DOCKER_CONTAINER} > /dev/null && echo "Removed container"
+
+    rm $PASSWD_FILE
+    rm $GROUP_FILE
+}
+
+
 DOCKER_DIR=$(pwd)
+
+get_hidra_version
+
+CENTOS_VERSION=7
+
+MAPPED_DIR=/tmp/hidra_builds/centos${CENTOS_VERSION}/${HIDRA_VERSION}/rpmbuild
+IN_DOCKER_DIR=~/rpmbuild
 
 if [ ! -d "$MAPPED_DIR" ]; then
     mkdir -p $MAPPED_DIR
 fi
 
-if [ -d "$MAPPED_DIR/hidra" ]; then
-    rm -rf $MAPPED_DIR/hidra
-fi
-
-cd ${MAPPED_DIR}
-# create rpm structure
-mkdir -p ${MAPPED_DIR}/BUILD
-mkdir -p ${MAPPED_DIR}/BUILDROOT
-mkdir -p ${MAPPED_DIR}/RPMS
-mkdir -p ${MAPPED_DIR}/SOURCES
-mkdir -p ${MAPPED_DIR}/SPECS
-mkdir -p ${MAPPED_DIR}/SRPMS
+prepare_build
 
 # get sources
-git clone --branch "v$VERSION" https://github.com/hidra-org/hidra.git
+cd ${MAPPED_DIR}
+download_hidra
+
 cd hidra
-git archive --format tar --prefix=hidra-${VERSION}/ -o hidra-${VERSION}.tar.gz v${VERSION}
-mv hidra-${VERSION}.tar.gz ${MAPPED_DIR}/SOURCES
+git archive --format tar --prefix=hidra-${HIDRA_VERSION}/ -o hidra-${HIDRA_VERSION}.tar.gz v${HIDRA_VERSION}
+mv hidra-${HIDRA_VERSION}.tar.gz ${MAPPED_DIR}/SOURCES
 cp hidra.spec ${MAPPED_DIR}/SPECS
 
-# DOCKER
-DOCKER_IMAGE=centos_build
-DOCKER_CONTAINER=hidra_build_centos
+build_docker_image
+build_package
 
-cd ${DOCKER_DIR}
-if [[ "$(docker images -q ${DOCKER_IMAGE} 2> /dev/null)" == "" ]]; then
-    echo "Creating container"
-    docker build -f ./Dockerfile.build_centos -t ${DOCKER_IMAGE} .
-fi
-
-cmd="cd ~/rpmbuild/SPECS; rpmbuild -ba -D 'dist .el7' hidra.spec"
-
-PASSWD_FILE=/tmp/passwd_x
-GROUP_FILE=/tmp/group_x
-
-getent passwd $USER > $PASSWD_FILE
-echo "$(id -gn):*:$(id -g):$USER" > $GROUP_FILE
-docker create -it \
-    -v $PASSWD_FILE:/etc/passwd \
-    -v $GROUP_FILE:/etc/group \
-    --userns=host \
-    --net=host \
-    --security-opt no-new-privileges \
-    --privileged \
-    -v ${MAPPED_DIR}:$IN_DOCKER_DIR \
-    --user=$(id -u $USER):$(id -g $USER) \
-    --name ${DOCKER_CONTAINER} \
-    ${DOCKER_IMAGE} \
-    bash
-docker start ${DOCKER_CONTAINER} > /dev/null && echo "Started container"
-docker exec --user=$(id -u $USER):$(id -g $USER) ${DOCKER_CONTAINER} sh -c "$cmd" && success=true
-docker stop ${DOCKER_CONTAINER} > /dev/null && echo "Stopped container"
-docker rm ${DOCKER_CONTAINER} > /dev/null && echo "Removed container"
-
-rm $PASSWD_FILE
-rm $GROUP_FILE
-
+# clean up
 #docker rmi ${DOCKER_IMAGE}
 #rm -rf ${MAPPED_DIR}/hidra
 
