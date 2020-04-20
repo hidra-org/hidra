5129063695e7ee1b00dc46a14b9290134790508d
Manuela Kuhn <manuela.kuhn@desy.de>
2020-04-20 09:58:46 +0200

    Add tests for track option in zmq sending

---
+++ test/coding_tests/zmq_test/track_tests/run_track_with_threshhold.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+import zmq
+import time
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.PUSH)
+    socket.copy_threshold = 0
+    socket.connect("tcp://localhost:50300")
+
+    print("using zmq version", zmq.__version__)
+    print("using libzmq version", zmq.zmq_version())
+
+    message = b"World"
+    print("Send: ", message)
+    res = socket.send(message, copy=False, track=True)
+
+    if res.done:
+        print("res: done")
+    else:
+        print("res: waiting")
+        res.wait()
+        print("res: waiting...")
+
+if __name__ == "__main__":
+    main()
