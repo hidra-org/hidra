5b4c16ff986e9031705ccbd6dc1020310252d388
Manuela Kuhn <manuela.kuhn@desy.de>
2020-05-26 12:40:56 +0200

    Add coding tests for zmq with ipv6

---
+++ test/coding_tests/zmq_test/ipv6/pushpull_send.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import zmq
+import time
+import socket as socket_m
+
+
+def main():
+    port = "50300"
+    ip = socket_m.getfqdn()
+
+    context = zmq.Context()
+    socket = context.socket(zmq.PUSH)
+    socket.ipv6 = True
+    socket.connect("tcp://{0}:{1}".format(ip, port))
+
+    print("using zmq version", zmq.__version__)
+
+    while True:
+        message = b"World"
+        print("Send: ", message)
+        res = socket.send(message, copy=False, track=True)
+
+        if res.done:
+            print("res: done")
+        else:
+            print("res: waiting")
+            res.wait()
+            print("res: waiting...")
+        print("sleeping...")
+        time.sleep(1)
+        print("sleeping...done")
+
+
+if __name__ == "__main__":
+    main()
