41bcc0bb45309347ea2488d792a331cf6de04c00
Malik Abdullah <muhammad.abdullah.malik@cfel.de>
2022-04-06 19:16:08 +0200

    Ctype wrapper is used to call getnetgrent for the lists of host

---
+++ src/api/python/hidra/utils/utils_netgroup.py
@@ -0,0 +1,31 @@
+
+from ctypes import *
+
+def find_hostname(netgroup):
+
+    libc = CDLL("libc.so.6")
+    if (libc != None):
+        pass
+    else:
+        libc = CDLL("libc.so.7")
+
+ 
+    hosts = None
+    if libc.setnetgrent(netgroup):
+        # returns 1 if successful and 0 otherwise
+        host = c_char_p()
+        user = c_char_p()
+        domain = c_char_p()
+        
+        hosts = []
+        while libc.getnetgrent(byref(host), byref(user), byref(domain)):
+            if host:
+                # check is host is not a NULL pointer
+                hosts.append(host.value)
+    
+    return(hosts)
+
+
+hosts = find_hostname(b"a3p62-hosts")
+print(hosts)
+print("done")
