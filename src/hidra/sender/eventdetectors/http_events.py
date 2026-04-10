hidra-org/hidra

commit 8c742f57d909ca4149e8926593e5b5c247be7bad

Reduce log level for connection and http errors
Do not flood the logs with error messages when the detector is just
offline. There is nothing that can be done on the Hidra side.

src/hidra/sender/eventdetectors/http_events.py
@@ -207,11 +207,20 @@ class EventDetectorImpl:
     def get_new_event(self):
         """Implementation of the abstract method get_new_event.
         """
+        files_stored = []
         try:
             files_stored = self._get_files_stored()
+        except requests.ConnectionError as err:
+            # Most likely cause is that the detector is offline
+            self.log.info("Could not connect to %s: %s", self.file_writer_url, err)
+        except requests.HTTPError as err:
+            # HTTP errors 404 and 500 seem to sometimes happen temporarily
+            self.log.warning("Could not get file list from %s: %s", self.file_writer_url, err)
         except Exception:
             self.log.error("Error in getting file list from %s",
                            self.file_writer_url, exc_info=True)
+
+        if not files_stored:
             # Wait till next try to prevent denial of service
             time.sleep(self.sleep_time)
             return []

commit 16c94c7bfe64ebfc72540aa28ba572babe874a91

Fix recently introduced http events error
Commit 8c742f57d909ca4149e8926593e5b5c247be7bad introduced an error
which resulted in new files with the same name as a previous one being
ignored.

The error was caused by an early return in case of an empty list
returned from the server. The file filter needs to see empty lists to
accurately track files that have been downloaded.

src/hidra/sender/eventdetectors/http_events.py
@@ -207,7 +207,7 @@ class EventDetectorImpl:
     def get_new_event(self):
         """Implementation of the abstract method get_new_event.
         """
-        files_stored = []
+        files_stored = None
         try:
             files_stored = self._get_files_stored()
         except requests.ConnectionError as err:
@@ -220,8 +220,10 @@ class EventDetectorImpl:
             self.log.error("Error in getting file list from %s",
                            self.file_writer_url, exc_info=True)
 
-        if not files_stored:
-            # Wait till next try to prevent denial of service
+        # Note: Do not return early in case of an empty list. The file filter needs to
+        # see empty lists to decide when a file was downloaded
+        if files_stored is None:
+            # There was an error. Wait till next try to prevent denial of service
             time.sleep(self.sleep_time)
             return []
 